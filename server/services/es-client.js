5f0ae33d73cd8e12f84047b18e54f04e60bc148a
c-adrien <83250257+c-adrien@users.noreply.github.com>
2024-12-16 14:22:01 +0100

    Change client to localhost

--- server/services/es-client.js
+++ server/services/es-client.js
@@ -1,7 +1,7 @@
 import elasticsearch from '@elastic/elasticsearch';
 
 const esClient = new elasticsearch.Client({
-  node: 'http://54.36.103.123:9200',
+  node: 'http://localhost:9200',
   log: 'info',
   apiVersion: '7.5',
   maxSockets: 20,
